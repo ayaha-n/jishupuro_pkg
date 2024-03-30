b71833456667b92862d0b83ad401fff46d73d939
ayaha-n <nagata-ayaha707@g.ecc.u-tokyo.ac.jp>
2024-03-30 09:43:37 +0000

    added discription

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,15 +5,18 @@ cd ~/catkin_ws/src/jishupuro_pkg
 # launch ファイルを実行
 roslaunch jishupuro_pkg capture.launch &
 #bash ~/rcb4eus/radxa/speak-jp/run-speak-jp.shの内容
+#MACアドレスを指定
 roslaunch stereo_image_sandbox speech_recognition_radxa.launch device:="\"bluealsa:DEV=64:B7:08:82:B6:12,PROFILE=a2dp\"" &
 
 # 各シリアルノードを実行
+# ポート名はあらかじめ固定しておく
 rosrun rosserial_python serial_node.py _port:=/dev/ttyACM-righteye _baud:=57600 __name:=b &
 rosrun rosserial_python serial_node.py _port:=/dev/ttyACM-lefteye _baud:=57600 __name:=a &
 rosrun rosserial_python serial_node.py _port:=/dev/ttyACM-m5stack _baud:=57600 __name:=c &
 
 # bluetoothスピーカの立ち上げ
 # python ~/rcb4eus/radxa/speak-jp/make_green.pyの内容
+#MACアドレスを指定
 # Pythonプログラムを実行してブロックする
 python3 -c "
 from bluezero import adapter
