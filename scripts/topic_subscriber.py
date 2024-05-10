5f624d11f7a01a6e9da82ba6e568dd0612e7cffd
ayaha-n <nagata-ayaha707@g.ecc.u-tokyo.ac.jp>
2024-05-10 13:32:18 +0900

    added message when START is pressed

--- scripts/topic_subscriber.py
+++ scripts/topic_subscriber.py
@@ -11,6 +11,7 @@ def topic_callback(msg):
     global process  # グローバル変数を参照することを明示
     # トピックのメッセージに応じてPythonスクリプトを制御
     if msg.data == "start_hume":
+        print("start")
         # subprocessモジュールなどを使用してPythonスクリプトを実行する
         # Pythonスクリプトを起動するコマンド
         command = ['python3', '/home/rock/catkin_ws/src/jishupuro_pkg/scripts/hume_sound_denden.py']
@@ -21,6 +22,7 @@ def topic_callback(msg):
         if process is not None:
         # hume_sound_ros_radxa.pyの停止
         # subprocessモジュールなどを使用してPythonスクリプトを終了する
+            print("stop")
             process.terminate()
             eye_status_pub = rospy.Publisher('/eye_status', UInt16, queue_size=1)
             eye_status_pub.publish(UInt16(0))
